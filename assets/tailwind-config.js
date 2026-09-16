185c1249e9f74a8610b6e1f09fe641f5aa48e039
nh8030954-art <nh8030954@gmail.com>
2026-09-16 10:22:47 +0300

    Move Tailwind config to first-party script

---
+++ assets/tailwind-config.js
@@ -0,0 +1,8 @@
+tailwind.config = {
+            theme: {
+                extend: {
+                    fontFamily: { sans: ['Assistant', 'sans-serif'], serif: ['Frank Ruhl Libre', 'serif'] },
+                    colors: { brand: { 50: '#f0fdfa', 100: '#ccfbf1', 500: '#14b8a6', 600: '#0d9488', 700: '#0f766e', 900: '#134e4a', dark: '#0f172a' } }
+                }
+            }
+        }
